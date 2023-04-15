e87f8877b6b7583e9e778eb7ec1a19c3fd703041
Mikalai Rusakovich <kolyos83@mail.ru>
2023-04-15 15:00:12 +0300

    feat: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  let number = JSON.stringify(backyard).split('"^^"').length - 1;
+  return number;
 }
 
 module.exports = {
